389667254d566721b9fd64de1798001df9ae54bd
huynmb2004 <lqngoc38@login01>
2026-06-23 08:36:23 +0700

    update loading + saving checkpoint and time limit

--- run_baseline.sh
+++ run_baseline.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=siglip_baseline
 #SBATCH --partition=batch
+#SBATCH --time=2-00:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
@@ -9,4 +10,6 @@
 
 source ~/miniconda3/bin/activate
 conda activate sinhvien_env
+
+# Chạy file train (lưu ý cờ resume nếu có)
 python3 train.py
